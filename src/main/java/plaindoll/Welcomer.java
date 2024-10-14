3a76001fb1cbfcded4c4742763c96a190d9cd239
VIKTOR <sharvik22@mail.ru>
2024-10-14 22:58:55 +0300

    Add too feature: reply functionality

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHelloween(){
+		return "Hunting hunter was hunted by his arrogance";
+	}
 }
